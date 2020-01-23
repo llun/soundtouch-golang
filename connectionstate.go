93eef04265f43ba5d374dec38ffb9b0f246c1cdd
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-01-23 20:06:59 +0100

    added Stringer functionality

--- connectionstate.go
+++ connectionstate.go
@@ -1,8 +1,33 @@
 package soundtouch
 
+import "fmt"
+
+const (
+	WIFI = "NETWORK_WIFI_CONNECTED"
+)
+
 type ConnectionStateUpdated struct {
 	// state="NETWORK_WIFI_CONNECTED" up="true" signal="MARGINAL_SIGNAL"
 	State  string `xml:"state,attr"`
 	Up     string `xml:"up,attr"`
 	Signal string `xml:"signal,attr"`
+}
+
+func (c ConnectionStateUpdated) String() string {
+
+	return fmt.Sprintf("Connection: {%v, %v = %v}",
+		func() string {
+			if c.State == WIFI {
+				return "WIFI"
+			} else {
+				return "NETWORK"
+			}
+		}(),
+		func() string {
+			if c.Up == "true" {
+				return "UP"
+			} else {
+				return "DOWN"
+			}
+		}(), c.Signal)
 }
