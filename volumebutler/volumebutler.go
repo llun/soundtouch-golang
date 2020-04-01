019074fae36cba0635981f672c2f1ec5760bfdb1
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-04-01 22:17:16 +0200

    enhanced scanning for soundtouch systems.

--- volumebutler/volumebutler.go
+++ volumebutler/volumebutler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 	"sync"
+	"time"
 
 	"github.com/jpillora/opts"
 	scribble "github.com/nanobox-io/golang-scribble"
@@ -21,10 +22,11 @@ var VERSION = "0.0" + version + "-src"
 const shortUsage = "Auto adjust volumes for special media on specific Soundtouch speakers."
 
 type config struct {
-	Speakers  []string  `opts:"group=Soundtouch" help:"Speakers to listen for, all if not set"`
-	Interface string    `opts:"group=Soundtouch" help:"network interface to listen"`
-	LogLevel  log.Level `help:"Log level, one of panic, fatal, error, warn or warning, info, debug, trace"`
-	Filename  string    `opts:"group=VolumeButler" help:"Where to store the data"`
+	Speakers            []string  `opts:"group=Soundtouch" help:"Speakers to listen for, all if not set"`
+	Interface           string    `opts:"group=Soundtouch" help:"network interface to listen"`
+	NoSoundtouchSystems int       `opts:"group=Soundtouch" help:"Number of Soundtouch systems to scan for."`
+	LogLevel            log.Level `help:"Log level, one of panic, fatal, error, warn or warning, info, debug, trace"`
+	Filename            string    `opts:"group=VolumeButler" help:"Where to store the data"`
 }
 type speakerMap map[string]bool
 
@@ -33,9 +35,10 @@ var visibleSpeakers = make(magicspeaker.MagicSpeakers)
 
 func main() {
 	conf = config{
-		LogLevel:  log.DebugLevel,
-		Interface: "en0",
-		Filename:  "./Volumes.db",
+		NoSoundtouchSystems: -1,
+		LogLevel:            log.DebugLevel,
+		Interface:           "en0",
+		Filename:            "./Volumes.db",
 	}
 
 	//parse config
@@ -49,43 +52,54 @@ func main() {
 
 	iff, filteredSpeakers, scribbleDb, _ := processConfig(conf)
 
-	speakerCh := soundtouch.Lookup(iff)
 	var wg sync.WaitGroup
-	messageCh := make(chan *soundtouch.Update)
+	log.Infof("Scanning for Soundtouch systems.")
+	for ok := true; ok; ok = (len(visibleSpeakers) < conf.NoSoundtouchSystems) {
 
-	for speaker := range speakerCh {
-		speakerInfo, _ := speaker.Info()
-		speaker.DeviceInfo = speakerInfo
-		ms := magicspeaker.New(speaker)
+		speakerCh := soundtouch.Lookup(iff)
+		messageCh := make(chan *soundtouch.Update)
 
-		spkLogger := log.WithFields(log.Fields{
-			"Speaker": speaker.DeviceInfo.Name,
-			"ID":      speaker.DeviceInfo.DeviceID,
-		})
+		for speaker := range speakerCh {
+			speakerInfo, _ := speaker.Info()
+			speaker.DeviceInfo = speakerInfo
 
-		// check wether we might have to ignore the speaker
-		if len(filteredSpeakers) > 0 && !(filteredSpeakers)[speakerInfo.Name] {
-			// spkLogger.Traceln("Seen but ignoring messages from: ", speakerInfo.Name)
-			continue
+			spkLogger := log.WithFields(log.Fields{
+				"Speaker": speaker.DeviceInfo.Name,
+				"ID":      speaker.DeviceInfo.DeviceID,
+			})
+
+			if checkInMap(speaker.DeviceInfo.DeviceID, visibleSpeakers) {
+				spkLogger.Debugf("Already included. Ignoring.")
+				continue
+			}
+
+			ms := magicspeaker.New(speaker)
+
+			// check wether we might have to ignore the speaker
+			if len(filteredSpeakers) > 0 && !(filteredSpeakers)[speakerInfo.Name] {
+				// spkLogger.Traceln("Seen but ignoring messages from: ", speakerInfo.Name)
+				continue
+			}
+
+			visibleSpeakers[speaker.DeviceInfo.DeviceID] = ms
+			spkLogger.Infof("Listening\n")
+			spkLogger.Debugf(" with IP: %v", speaker.IP)
+			wg.Add(1)
+
+			go func(s *soundtouch.Speaker, msgChan chan *soundtouch.Update) {
+				defer wg.Done()
+				webSocketCh, _ := s.Listen()
+				magicSpeaker := magicspeaker.New(s)
+				magicSpeaker.ScribbleDb = scribbleDb
+				magicSpeaker.WebSocketCh = webSocketCh
+				magicSpeaker.SpeakerName = visibleSpeakers[ms.DeviceInfo.DeviceID].DeviceInfo.Name
+				magicSpeaker.MessageLoop()
+			}(speaker, messageCh)
+
 		}
-
-		visibleSpeakers[speaker.DeviceInfo.DeviceID] = ms
-		spkLogger.Infof("Listening\n")
-		spkLogger.Debugf(" with IP: %v", speaker.IP)
-		wg.Add(1)
-
-		go func(s *soundtouch.Speaker, msgChan chan *soundtouch.Update) {
-			defer wg.Done()
-			webSocketCh, _ := s.Listen()
-			magicSpeaker := magicspeaker.New(s)
-			magicSpeaker.ScribbleDb = scribbleDb
-			magicSpeaker.WebSocketCh = webSocketCh
-			magicSpeaker.SpeakerName = visibleSpeakers[ms.DeviceInfo.DeviceID].DeviceInfo.Name
-			magicSpeaker.MessageLoop()
-		}(speaker, messageCh)
-
+		time.Sleep(10 * time.Second)
 	}
-
+	log.Infof("Found all Soundtouch systems. Normal Operation.")
 	wg.Wait()
 }
 
@@ -102,7 +116,7 @@ func processConfig(conf config) (*net.Interface, speakerMap, *scribble.Driver, e
 
 	for _, value := range conf.Speakers {
 		filteredSpeakers[value] = true
-		log.Tracef("Reacting only speakers %v\n", value)
+		log.Debugf("Reacting only speakers %v\n", value)
 	}
 
 	db, _ := scribble.New(conf.Filename, nil)
@@ -112,4 +126,13 @@ func processConfig(conf config) (*net.Interface, speakerMap, *scribble.Driver, e
 
 	log.Debugf("Created database: %v\n", db)
 	return i, filteredSpeakers, db, nil
+}
+
+func checkInMap(deviceID string, list magicspeaker.MagicSpeakers) bool {
+	for _, ms := range list {
+		if ms.Speaker.DeviceInfo.DeviceID == deviceID {
+			return true
+		}
+	}
+	return false
 }
