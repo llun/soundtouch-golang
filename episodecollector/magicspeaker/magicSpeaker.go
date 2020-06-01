134e5b6ec5134466eab6ff2c89a46f40f297097d
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-06-01 12:43:51 +0200

    episode collector implemented

--- episodecollector/magicspeaker/magicSpeaker.go
+++ episodecollector/magicspeaker/magicSpeaker.go
@@ -35,13 +35,12 @@ func (m *MagicSpeaker) ReadDB(album string, currentAlbum *DbEntry) *DbEntry {
 	if currentAlbum == nil {
 		currentAlbum = &DbEntry{}
 	}
-	m.ScribbleDb.Read(m.SpeakerName, album, &currentAlbum)
+	m.ScribbleDb.Read("All", album, &currentAlbum)
 	return currentAlbum
 }
 
 func (m *MagicSpeaker) WriteDB(album string, storedAlbum *DbEntry) {
 	storedAlbum.LastUpdated = time.Now()
-	m.ScribbleDb.Write(m.SpeakerName, album, &storedAlbum)
 	m.ScribbleDb.Write("All", album, &storedAlbum)
 }
 
@@ -51,13 +50,10 @@ func (m *MagicSpeaker) ReadAlbumDB(album string, updateMsg MagicUpdate) *DbEntry
 
 	if storedAlbum.AlbumName == "" {
 		// no, write this into the database
-		retrievedVol, _ := m.Volume()
 		storedAlbum.AlbumName = album
 		// HYPO: We are in observation window, then the current volume could also
 		// be a good measurement
-		storedAlbum.Volume = retrievedVol.TargetVolume
 		storedAlbum.DeviceID = updateMsg.DeviceID
-		storedAlbum.LastUpdated = time.Now()
 		storedAlbum.ContentItem = updateMsg.ContentItem()
 		m.WriteDB(album, storedAlbum)
 	}
@@ -119,28 +115,8 @@ func (m *MagicSpeaker) HandleUpdate(msg MagicUpdate, webSocketCh chan *soundtouc
 
 	// time window independend
 	// Do we know this album already?  - read from database
-	storedAlbum := m.ReadAlbumDB(album, msg)
+	m.ReadAlbumDB(album, msg)
 
-	// time window and speaker depended
-	// 		if available for this album
-	//			set the volume
-	if storedAlbum.Volume != 0 && time.Now().After(storedAlbum.LastUpdated.Add(20*time.Minute)) {
-		// Only setting a volume if it was last update more than 20 minutes ago
-		m.SetVolume(storedAlbum.Volume)
-	}
-
-	// wait for a minute and process last volume observed
-	// construct the mean value of current and past volumes
-	// store the update value
-	time.Sleep(60 * time.Second)
-
-	// clear message and keep last volume update
-	lastVolume := m.ScanForVolume()
-	m.ReadDB(album, storedAlbum)
-	if lastVolume != nil {
-		storedAlbum.Volume = storedAlbum.calcNewVolume(lastVolume.TargetVolume)
-		m.ScribbleDb.Write(m.SpeakerName, album, &storedAlbum)
-	}
 }
 
 func (db *DbEntry) calcNewVolume(currVolume int) int {
